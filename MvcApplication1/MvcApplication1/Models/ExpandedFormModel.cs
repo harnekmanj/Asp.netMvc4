bed699e724f3f5ebb70bf57416fa257585d7e0ca
Harnek Manj <harnek.manj@gmail.com>
2014-01-14 22:11:44 -0800

    Added Model Classes

---
+++ MvcApplication1/MvcApplication1/Models/ExpandedFormModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class ExpandedFormModel
+    {
+        private List<int> _digits = new List<int>();
+
+        public ExpandedFormModel()
+        {
+            System.Random random = new Random();
+            for (int i = 0; i <= 10; i++)
+            {
+                _digits.Add(random.Next(101, 9999));
+            }
+        }
+
+        public List<int> Digits
+        {
+            get
+            {
+                return _digits;
+            }
+        }
+
+        public int UnderlinePlace
+        {
+            get
+            {
+                Random random = new Random();
+                return random.Next(4);
+            }
+        }
+    }
+}
---
+++ MvcApplication1/MvcApplication1/Models/SubtractModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class SubtractModel
+    {
+        private List<Equation> _equations = new List<Equation>();
+
+        public SubtractModel()
+        {
+            System.Random random = new Random();
+            for (int i = 0; i <= 10; i++)
+            {
+                Equation e = new Equation();
+                e.Lhs = random.Next(65);
+                e.Rhs = random.Next(35);
+                if (e.Lhs > e.Rhs)
+                {
+                    _equations.Add(e);
+                }
+                else
+                {
+                    --i;
+                }
+            }
+        }
+
+        public List<Equation> Equations
+        {
+            get
+            {
+                return _equations;
+            }
+        }
+    }
+}
