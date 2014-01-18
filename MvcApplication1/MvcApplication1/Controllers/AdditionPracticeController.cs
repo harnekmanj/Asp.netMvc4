11921ead7407f6694e04603f0cd42a88d000498d
Harnek Manj <harnek.manj@gmail.com>
2014-01-17 16:44:10 -0800

    Added place value

---
+++ MvcApplication1/MvcApplication1/Controllers/AdditionPracticeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class AdditionPracticeController : Controller
+    {
+        //
+        // GET: /AdditionPractice/
+
+		 public ActionResult AdditionPractice()
+		 {
+			 Models.AdditionModel add = new Models.AdditionModel();
+			 System.Random random = new Random();
+			 add.Rhs = random.Next(15);
+			 add.Lhs = random.Next(25);
+			 return View(add);
+		 }
+
+    }
+}
